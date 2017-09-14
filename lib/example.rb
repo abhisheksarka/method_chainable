775b81a6efa15870f21270aaa9e731c808e438b2
abhisheksarka <abhisheksarka@gmail.com>
2017-09-14 12:36:30 +0530

    added arity check

---
+++ lib/example.rb
@@ -0,0 +1,16 @@
+class Example
+  extend MethodChainable
+  class << self
+    def hello
+      'hello'
+    end
+
+    def space(s)
+      s + ' '
+    end
+
+    def world(s)
+      s + 'world'
+    end
+  end
+end
--- lib/method_chainable/proxy.rb
+++ lib/method_chainable/proxy.rb
@@ -19,13 +19,14 @@ module MethodChainable
       else
         if m_chainable.respond_to? m
           arg = (@m_chain_count == 0) ? input : output
+          arity = m_chainable.method(m).arity
 
-          if args.count > 0
+          if arity > 0
             @output = m_chainable.send(m, arg)
           else
             @output = m_chainable.send(m)
           end
-          
+
           @m_chain_count += 1
 
           return self
